8b23b9fe89b111a449cc2870c31c242ba594108b
geun <geun@touchingsignal.com>
2014-03-23 09:50:52 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoveRailsSampleApp::Application.config.secret_key_base = '18dcf5f38695bcb89b2d1e0897c8e9cecd1cf950df1ee801a9d0c596691a5093492ef1763b5380d56320aefe97d6485f94aa4e8ba15124119851510661c84d71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DoveRailsSampleApp::Application.config.session_store :cookie_store, key: '_dove-rails-sample-app_session'
